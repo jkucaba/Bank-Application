28ade6d7e01af2d16daef5c31e4eceb3281e55d4
Jakub <jakub.kucaba@gmail.com>
2023-10-26 13:48:53 +0200

    Creating Stages

--- src/main/java/com/jakubku/mazebank/mazebank/models/Model.java
+++ src/main/java/com/jakubku/mazebank/mazebank/models/Model.java
@@ -1,4 +1,21 @@
 package com.jakubku.mazebank.mazebank.models;
 
+import com.jakubku.mazebank.mazebank.views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+    private Model(){
+        this.viewFactory = new ViewFactory();
+    }
+    public static synchronized Model getInstance(){
+        if(model == null){
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
--- src/main/java/com/jakubku/mazebank/mazebank/views/ViewFactory.java
+++ src/main/java/com/jakubku/mazebank/mazebank/views/ViewFactory.java
@@ -1,4 +1,47 @@
 package com.jakubku.mazebank.mazebank.views;
 
+import com.jakubku.mazebank.mazebank.controllers.client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    //Client Views
+    private AnchorPane dashboardView;
+    public ViewFactory(){}
+    public AnchorPane getDashboardView(){
+        if(dashboardView == null){
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/fxml/client/dashboard.fxml")).load();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+    public void showLoginWindow(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/client/client.fxml"));
+        ClientController clientController = new ClientController();
+        loader.setController(clientController);
+        createStage(loader);
+    }
+    private void createStage(FXMLLoader loader){
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Maze Bank");
+        stage.show();
+    }
+
 }
