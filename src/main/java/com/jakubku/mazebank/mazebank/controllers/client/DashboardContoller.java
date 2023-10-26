9e8f60c6344c8f9d5c3bcd89e3af7f7e6e8ae9c1
Jakub <jakub.kucaba@gmail.com>
2023-10-26 11:55:38 +0200

    Dashboard View

---
+++ src/main/java/com/jakubku/mazebank/mazebank/controllers/client/DashboardContoller.java
@@ -0,0 +1,20 @@
+package com.jakubku.mazebank.mazebank.controllers.client;
+
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+public class DashboardContoller {
+    public Text user_name;
+    public Label login_date;
+    public Label checking_bal;
+    public Label checking_acc_num;
+    public Label savings_bal;
+    public Label savings_acc_num;
+    public Label income_lbl;
+    public Label expense_lbl;
+    public ListView transaction_listview;
+    public TextField payee_fld;
+    public TextField amount_fld;
+    public TextArea message_fld;
+    public Button send_money_btn;
+}
