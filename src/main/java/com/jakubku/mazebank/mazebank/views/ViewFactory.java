940f17634695fdf5f369b171d37e29ef033930e9
Jakub <jakub.kucaba@gmail.com>
2023-10-28 10:17:35 +0200

    Stage Icons & Maximize Button

--- src/main/java/com/jakubku/mazebank/mazebank/views/ViewFactory.java
+++ src/main/java/com/jakubku/mazebank/mazebank/views/ViewFactory.java
@@ -6,6 +6,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -140,6 +141,8 @@ public class ViewFactory {
         }
         Stage stage = new Stage();
         stage.setScene(scene);
+        stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/images/icon.png"))));
+        stage.setResizable(false);
         stage.setTitle("Maze Bank");
         stage.show();
     }
