6c61aadee007060770a9bf6abe00bb04d2c08721
Jakub <jakub.kucaba@gmail.com>
2023-11-01 16:33:59 +0100

    Viewing All Clients in Admin

--- src/main/java/com/jakubku/mazebank/mazebank/controllers/admin/ClientsController.java
+++ src/main/java/com/jakubku/mazebank/mazebank/controllers/admin/ClientsController.java
@@ -1,15 +1,26 @@
 package com.jakubku.mazebank.mazebank.controllers.admin;
 
+import com.jakubku.mazebank.mazebank.models.Client;
+import com.jakubku.mazebank.mazebank.models.Model;
+import com.jakubku.mazebank.mazebank.views.ClientCellFactory;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClientsController implements Initializable {
-    public ListView clients_listview;
+    public ListView<Client> clients_listview;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        initData();
+        clients_listview.setItems(Model.getInstance().getClients());
+        clients_listview.setCellFactory(e -> new ClientCellFactory());
+    }
 
+    private void initData() {
+        if(Model.getInstance().getClients().isEmpty()){
+            Model.getInstance().setClients();
+        }
     }
 }
