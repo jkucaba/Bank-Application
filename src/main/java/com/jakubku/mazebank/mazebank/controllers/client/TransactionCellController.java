87bd0af0a242dc4fd3f12ea3d673d8aa5809b038
Jakub <jakub.kucaba@gmail.com>
2023-11-03 19:04:41 +0100

    Client Transaction Message

--- src/main/java/com/jakubku/mazebank/mazebank/controllers/client/TransactionCellController.java
+++ src/main/java/com/jakubku/mazebank/mazebank/controllers/client/TransactionCellController.java
@@ -4,6 +4,7 @@ import com.jakubku.mazebank.mazebank.models.Model;
 import com.jakubku.mazebank.mazebank.models.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
@@ -16,6 +17,7 @@ public class TransactionCellController implements Initializable {
     public Label trans_date_lbl;
     public Label sender_lbl;
     public Label receiver_lbl;
+    public Button message_btn;
     public Label amount_lbl;
     private final Transaction transaction;
     public TransactionCellController(Transaction transaction){
@@ -28,6 +30,7 @@ public class TransactionCellController implements Initializable {
         receiver_lbl.textProperty().bind(transaction.receiverProperty());
         amount_lbl.textProperty().bind(transaction.amountProperty().asString());
         trans_date_lbl.textProperty().bind(transaction.dateProperty().asString());
+        message_btn.setOnAction(e -> Model.getInstance().getViewFactory().showMessageWindow(transaction.senderProperty().get(), transaction.messageProperty().get()));
         transactionIcons();
     }
 
